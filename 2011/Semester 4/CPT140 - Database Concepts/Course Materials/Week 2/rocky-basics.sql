5050e14b66faabd870dace1228eec01a811fa111
Peekay86 <glenn.chamberlain@gmail.com>
2011-12-07 12:06:11 +1100

    CPT110 A1 Details, CPT140 Week 2 Course Materials

---
+++ 2011/Semester 4/CPT140 - Database Concepts/Course Materials/Week 2/rocky-basics.sql
@@ -0,0 +1,43 @@
+/*
+ * Create a rocky concrete database without any integrity constraints. 
+ */  
+
+/* Remove any existing tables
+ * Customers, Products, Orders, Order_details
+ */
+ 
+drop table order_details;
+drop table orders;
+drop table products;
+drop table customers; 
+
+/*  Data Definition */
+ 
+CREATE TABLE CUSTOMERS
+        (CUST_NO        INTEGER         ,
+        CUST_NAME       VARCHAR(40)     ,
+        STREET          VARCHAR(40)     ,
+        TOWN            VARCHAR(40)     ,
+        POSTCODE        INTEGER         ,
+        CR_LIMIT        INTEGER         ,
+        CURR_BALANCE    INTEGER         );
+ 
+CREATE TABLE PRODUCTS
+        (PROD_COD       VARCHAR(10)     ,
+        DESCRIPTION     VARCHAR(50)     ,
+        PROD_GROUP      CHAR(1)         , 
+        LIST_PRICE      INTEGER         ,
+        QTY_ON_HAND     INTEGER         ,
+        REMAKE_LEVEL    INTEGER         ,
+        REMAKE_QTY      INTEGER         );
+ 
+CREATE TABLE ORDERS
+        (ORDER_NO       INTEGER         ,
+        ORDER_DATE      DATE            ,
+        CUST_NO         INTEGER         );
+ 
+CREATE TABLE ORDER_DETAILS
+        (ORDER_NO       INTEGER         ,
+        PROD_COD        VARCHAR(10)     ,
+        ORDER_QTY       INTEGER         ,
+        ORDER_PRICE     INTEGER         );
