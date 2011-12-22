9672d0c845387df71dfc62692ef86a1d73e34cde
Peekay86 <glenn.chamberlain@gmail.com>
2011-12-22 16:12:00 +1100

    CPT140 Week 4 work

---
+++ 2011/Semester 4/CPT140 - Database Concepts/Coursework/Week 4/S3356661.sql
@@ -0,0 +1,33 @@
+/* Question 1 */
+SELECT cust_name, street, town, postcode, cr_limit
+FROM Customers
+WHERE cust_name='Di Hunter';
+
+/* Question 2 */
+SELECT town, COUNT(*)
+FROM Customers
+GROUP BY Town;
+
+/* Question 3 */
+SELECT cust_name, street, town, postcode
+FROM Customers
+WHERE curr_balance >=(cr_limit - 50);
+
+/* Question 4 */
+SELECT prod_group, AVG(DISTINCT list_price)
+FROM Products;
+
+/* Question 5 */
+SELECT prod_cod, prod_group, SUM(list_price * qty_on_hand AS Total)
+FROM Products;
+
+SELECT prod_group, Total
+FROM Products
+HAVING Count(*) >=2000;
+
+/* Question 6 */
+SELECT prod_cod
+FROM Products
+WHERE Description LIKE %Trough% OR %Tank%;
+
+
