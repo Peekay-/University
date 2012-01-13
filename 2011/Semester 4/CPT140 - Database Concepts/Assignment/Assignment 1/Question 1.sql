ca23eb62ec09d6e5026f08acd9e2101e0c5a6ee8
Glenn Chamberlain <glenn.chamberlain@gmail.com>
2012-01-14 10:08:43 +1100

    CPT140 A01

--- 2011/Semester 4/CPT140 - Database Concepts/Assignment/Assignment 1/Question 1.sql
+++ 2011/Semester 4/CPT140 - Database Concepts/Assignment/Assignment 1/Question 1.sql
@@ -1,20 +1,39 @@
-/* Question 1 - Complete */
+/* Question 1 */
+
+/* Question 1 */
 SELECT title, length, rating
 FROM Movieinfo
 ORDER BY length;
 
-/* Question 2 - Complete */
+/* Question 2 */
 SELECT Director
 FROM Directinfo
 WHERE Director LIKE 'A%'
 GROUP BY Director;
 
-/* Question 3 - Complete */ 
+/* Question 3 */ 
 SELECT title
-from MovieInfo
-WHERE rating = 'G' AND (length >=90 OR year = 1990);
+FROM MovieInfo
+WHERE rating = 'G' AND (length >90 OR year = 1990);
 
 /* Question 4 */
+SELECT studio, count(*) as NumberOfFilms
+FROM Movieinfo
+GROUP BY studio
+ORDER BY count(*) desc;
+
+/* Question 5 */
+SELECT studio, count(*) as NumberOfFilms
+FROM Movieinfo
+GROUP BY studio
+HAVING count(*) = 1
+ORDER BY studio;
+
+
+
+
+
+
 
 
 
